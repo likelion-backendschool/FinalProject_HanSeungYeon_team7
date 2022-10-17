2905f4ad8fb2c94387f7ae4e8278b899f2cc7497
ahah525 <ahah525@naver.com>
2022-10-17 13:16:30 +0900

    Feat : project setting

---
+++ 1Week_Mission/mutbooks/src/main/java/com/example/mutbooks/MutbooksApplication.java
@@ -0,0 +1,13 @@
+package com.example.mutbooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MutbooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MutbooksApplication.class, args);
+	}
+
+}
