f22a37f1b1bb27c121e7a00b02498f6ed1ba64e2
ahah525 <ahah525@naver.com>
2022-10-17 15:21:27 +0900

    #3 - Feat: Member 엔티티 설계

---
+++ 1Week_Mission/mutbooks/src/main/java/com/example/mutbooks/app/member/entity/Member.java
@@ -0,0 +1,26 @@
+package com.example.mutbooks.app.member.entity;
+
+import com.example.mutbooks.app.base.entity.BaseEntity;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+@Entity
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString(callSuper = true)
+public class Member extends BaseEntity {
+    @Column(unique = true)
+    private String username;
+    @JsonIgnore
+    private String password;
+    private String nickname;
+    private String email;
+    private Integer authLevel;  // 권한레벨(3 = 일반, 7 = 관리자)
+}
