14b2081b95fa830269e8ea4252ac56383db8c52f
ahah525 <ahah525@naver.com>
2022-10-17 15:08:39 +0900

    #2 - Feat : BaseEntity 추가

--- 1Week_Mission/mutbooks/src/main/java/com/example/mutbooks/MutbooksApplication.java
+++ 1Week_Mission/mutbooks/src/main/java/com/example/mutbooks/MutbooksApplication.java
@@ -2,8 +2,10 @@ package com.example.mutbooks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class MutbooksApplication {
 
 	public static void main(String[] args) {
