46e747ea3562e3cbb781ed29715f3b16d05a5c3a
ahah525 <ahah525@naver.com>
2022-10-18 22:56:08 +0900

    #10 - Feat: 글 상세페이지 해시태그 표시

--- 1Week_Mission/mutbooks/src/main/java/com/example/mutbooks/app/post/entity/Post.java
+++ 1Week_Mission/mutbooks/src/main/java/com/example/mutbooks/app/post/entity/Post.java
@@ -1,13 +1,14 @@
 package com.example.mutbooks.app.post.entity;
 
 import com.example.mutbooks.app.base.entity.BaseEntity;
+import com.example.mutbooks.app.hashTag.entity.PostHashTag;
 import com.example.mutbooks.app.member.entity.Member;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
 
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -21,4 +22,21 @@ public class Post extends BaseEntity {
 
     @ManyToOne(fetch = FetchType.LAZY)
     private Member author;  // 글쓴이
+
+    @OneToMany(mappedBy = "post", cascade = {CascadeType.ALL})
+    private List<PostHashTag> hashTags = new ArrayList<>();
+
+    // 해당 게시글의 해시태그들을 한 문장으로 반환
+//    public String getHashTagString() {
+//        if(hashTags.isEmpty()) {
+//            return "";
+//        }
+//
+//        return "#" + hashTags
+//                .stream()
+//                .map(hashTag -> hashTag.getPostKeyword().getContent())
+//                .sorted()
+//                .collect(Collectors.joining(" #"))
+//                .trim();
+//    }
 }
