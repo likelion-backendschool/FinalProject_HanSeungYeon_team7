a8d79a78020d7f0a73b1c3c32a546ffb037249b7
ahah525 <ahah525@naver.com>
2022-10-18 11:40:55 +0900

    #7 - Feat: user2 글 2개 작성(Init 데이터 추가)

--- 1Week_Mission/mutbooks/src/main/java/com/example/mutbooks/app/base/initData/DevInitData.java
+++ 1Week_Mission/mutbooks/src/main/java/com/example/mutbooks/app/base/initData/DevInitData.java
@@ -1,6 +1,7 @@
 package com.example.mutbooks.app.base.initData;
 
 import com.example.mutbooks.app.member.service.MemberService;
+import com.example.mutbooks.app.post.service.PostService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,9 +11,9 @@ import org.springframework.context.annotation.Profile;
 @Profile("dev")
 public class DevInitData implements InitDataBefore {
     @Bean
-    CommandLineRunner initData(MemberService memberService) {
+    CommandLineRunner initData(MemberService memberService, PostService postService) {
         return args -> {
-            before(memberService);
+            before(memberService, postService);
         };
     }
 }
--- 1Week_Mission/mutbooks/src/main/java/com/example/mutbooks/app/base/initData/InitDataBefore.java
+++ 1Week_Mission/mutbooks/src/main/java/com/example/mutbooks/app/base/initData/InitDataBefore.java
@@ -3,12 +3,19 @@ package com.example.mutbooks.app.base.initData;
 import com.example.mutbooks.app.member.entity.Member;
 import com.example.mutbooks.app.member.form.JoinForm;
 import com.example.mutbooks.app.member.service.MemberService;
+import com.example.mutbooks.app.post.form.WriteForm;
+import com.example.mutbooks.app.post.service.PostService;
 
 public interface InitDataBefore {
-    default void before(MemberService memberService) {
-        // 일반 회원
-        Member member1 = memberService.join(new JoinForm("user1", "1234", null, "user1@test.com"));
+    default void before(MemberService memberService, PostService postService) {
         // 작가 회원
-        Member member2 = memberService.join(new JoinForm("user2", "1234", "작가", "user2@test.com"));
+        Member member1 = memberService.join(new JoinForm("user1", "1234", "작가", "user1@test.com"));
+        // 일반 회원
+        Member member2 = memberService.join(new JoinForm("user2", "1234", null, "user2@test.com"));
+
+        // 글 작성
+        postService.write(member1, new WriteForm("제목1", "원문 내용1", null));
+        postService.write(member1, new WriteForm("제목2", "원문 내용2", null));
+
     }
 }
