8322f7449529a76f2340663848671668d8ccbecf
ahah525 <ahah525@naver.com>
2022-10-18 16:29:09 +0900

    #6 - Test: contentHtml 포함해서 글 수정 테스트, 글 삭제 테스트 추가

--- 1Week_Mission/mutbooks/src/test/java/com/example/mutbooks/app/post/service/PostServiceTest.java
+++ 1Week_Mission/mutbooks/src/test/java/com/example/mutbooks/app/post/service/PostServiceTest.java
@@ -4,6 +4,7 @@ import com.example.mutbooks.app.member.entity.Member;
 import com.example.mutbooks.app.member.repository.MemberRepository;
 import com.example.mutbooks.app.post.entity.Post;
 import com.example.mutbooks.app.post.form.WriteForm;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,10 +28,11 @@ class PostServiceTest {
     @Test
     @DisplayName("글 작성")
     void t1() {
+        // given
         Member author = memberRepository.findByUsername("user2").orElse(null);
-
+        // when
         Post post = postService.write(author, new WriteForm("제목", "안녕하세요1", "<ul><li><p>안녕하세요1</p></li></ul>", null));
-
+        // then
         assertThat(post).isNotNull();
         assertThat(post.getAuthor().getUsername()).isEqualTo("user2");
         assertThat(post.getSubject()).isEqualTo("제목");
@@ -41,13 +43,28 @@ class PostServiceTest {
     @Test
     @DisplayName("글 수정")
     void t2() {
+        // given
         Member author = memberRepository.findByUsername("user2").orElse(null);
-
         Post post = postService.findById(1);
-        postService.modify(post, new WriteForm("new 제목", "new 마크다운 원문 내용", null, null));
-
+        // when
+        postService.modify(post, new WriteForm("new 제목", "new 안녕하세요", "<ul><li><p>new 안녕하세요</p></li></ul>", null));
+        // then
         assertThat(post).isNotNull();
         assertThat(post.getSubject()).isEqualTo("new 제목");
-        assertThat(post.getContent()).isEqualTo("new 마크다운 원문 내용");
+        assertThat(post.getContent()).isEqualTo("new 안녕하세요");
+        assertThat(post.getContentHtml()).isEqualTo("<ul><li><p>new 안녕하세요</p></li></ul>");
+    }
+
+    @Test
+    @DisplayName("글 삭제")
+    void t3() {
+        // given
+        Post post = postService.findById(1);
+        // when
+        postService.delete(post);
+        // then
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            postService.findById(1);
+        });
     }
 }
