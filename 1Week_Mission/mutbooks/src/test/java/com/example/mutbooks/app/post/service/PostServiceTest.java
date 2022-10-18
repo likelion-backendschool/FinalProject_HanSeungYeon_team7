3598c7d23dd7726dfaa22b1ff4e199d4277c72c2
ahah525 <ahah525@naver.com>
2022-10-19 00:01:45 +0900

    #10 - Test: 글작성, 수정 테스트에 해시태그 작성, 수정 테스트 추가

--- 1Week_Mission/mutbooks/src/test/java/com/example/mutbooks/app/post/service/PostServiceTest.java
+++ 1Week_Mission/mutbooks/src/test/java/com/example/mutbooks/app/post/service/PostServiceTest.java
@@ -1,5 +1,7 @@
 package com.example.mutbooks.app.post.service;
 
+import com.example.mutbooks.app.hashTag.entity.PostHashTag;
+import com.example.mutbooks.app.hashTag.service.HashTagService;
 import com.example.mutbooks.app.member.entity.Member;
 import com.example.mutbooks.app.member.repository.MemberRepository;
 import com.example.mutbooks.app.post.entity.Post;
@@ -11,6 +13,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -24,35 +28,49 @@ class PostServiceTest {
     @Autowired
     private MemberRepository memberRepository;
 
+    @Autowired
+    private HashTagService hashTagService;
 
     @Test
     @DisplayName("글 작성")
     void t1() {
         // given
         Member author = memberRepository.findByUsername("user2").orElse(null);
+        String keywords = "#로맨스 #판타지 #판타지";
         // when
-        Post post = postService.write(author, new WriteForm("제목", "안녕하세요1", "<ul><li><p>안녕하세요1</p></li></ul>", null));
+        Post post = postService.write(author, new WriteForm("제목", "안녕하세요1", "<ul><li><p>안녕하세요1</p></li></ul>", keywords));
         // then
         assertThat(post).isNotNull();
         assertThat(post.getAuthor().getUsername()).isEqualTo("user2");
         assertThat(post.getSubject()).isEqualTo("제목");
         assertThat(post.getContent()).isEqualTo("안녕하세요1");
         assertThat(post.getContentHtml()).isEqualTo("<ul><li><p>안녕하세요1</p></li></ul>");
+        // 해시태그
+        List<PostHashTag> hashTags = hashTagService.findByPostId(3);
+        assertThat(hashTags.size()).isEqualTo(2);
+        assertThat(hashTags.get(0).getPostKeyword().getContent()).isEqualTo("로맨스");
+        assertThat(hashTags.get(1).getPostKeyword().getContent()).isEqualTo("판타지");
     }
 
     @Test
     @DisplayName("글 수정")
     void t2() {
         // given
-        Member author = memberRepository.findByUsername("user2").orElse(null);
         Post post = postService.findById(1);
+        String keywords = "#로맨스 #판타지 #소설";
         // when
-        postService.modify(post, new WriteForm("new 제목", "new 안녕하세요", "<ul><li><p>new 안녕하세요</p></li></ul>", null));
+        postService.modify(post, new WriteForm("new 제목", "new 안녕하세요", "<ul><li><p>new 안녕하세요</p></li></ul>", keywords));
         // then
         assertThat(post).isNotNull();
         assertThat(post.getSubject()).isEqualTo("new 제목");
         assertThat(post.getContent()).isEqualTo("new 안녕하세요");
         assertThat(post.getContentHtml()).isEqualTo("<ul><li><p>new 안녕하세요</p></li></ul>");
+        // 해시태그
+        List<PostHashTag> hashTags = hashTagService.findByPostId(1);
+        assertThat(hashTags.size()).isEqualTo(3);
+        assertThat(hashTags.get(0).getPostKeyword().getContent()).isEqualTo("판타지");
+        assertThat(hashTags.get(1).getPostKeyword().getContent()).isEqualTo("소설");
+        assertThat(hashTags.get(2).getPostKeyword().getContent()).isEqualTo("로맨스");
     }
 
     @Test
