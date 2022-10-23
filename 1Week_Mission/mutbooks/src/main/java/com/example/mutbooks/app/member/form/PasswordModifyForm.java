3646d78d6ef774983022894e5422083c1e96446e
ahah525 <ahah525@naver.com>
2022-10-23 23:30:37 +0900

    #13 - Refactor : 회원 관련 폼 객체 값 검증 및 에러 메시지 추가

--- 1Week_Mission/mutbooks/src/main/java/com/example/mutbooks/app/member/form/PasswordModifyForm.java
+++ 1Week_Mission/mutbooks/src/main/java/com/example/mutbooks/app/member/form/PasswordModifyForm.java
@@ -4,14 +4,20 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 @Getter
 @Setter
 public class PasswordModifyForm {
+    @Size(min = 8, max = 16, message = "비밀번호는 8 ~ 16자리로 입력해주세요.")
     @NotEmpty(message = "현재 비밀번호를 입력해주세요.")
     private String password;            // 현재 비밀번호
+
+    @Size(min = 8, max = 16, message = "비밀번호는 8 ~ 16자리로 입력해주세요.")
     @NotEmpty(message = "새 비밀번호를 입력해주세요.")
     private String newPassword;         // 새 비밀번호
+
+    @Size(min = 8, max = 16, message = "비밀번호는 8 ~ 16자리로 입력해주세요.")
     @NotEmpty(message = "새 비밀번호 확인을 입력해주세요.")
     private String newPasswordConfirm;  // 새 비밀번호 확인
 }
