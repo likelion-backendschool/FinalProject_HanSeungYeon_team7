7374ee102ca134dd1eb937108b1d3b1951ed229a
ahah525 <ahah525@naver.com>
2022-10-24 10:25:32 +0900

    #16 - Feat: Product 엔티티 설계

---
+++ 2Week_Mission/mutbooks/src/main/java/com/example/mutbooks/app/product/entity/Product.java
@@ -0,0 +1,33 @@
+package com.example.mutbooks.app.product.entity;
+
+import com.example.mutbooks.app.base.entity.BaseEntity;
+import com.example.mutbooks.app.keyword.entity.PostKeyword;
+import com.example.mutbooks.app.member.entity.Member;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@SuperBuilder
+@NoArgsConstructor
+@ToString(callSuper = true)
+public class Product extends BaseEntity {
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member author;  // 작가
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private PostKeyword postKeyword;    // 게시글 키워드
+
+    private String subject;     // (노출용)상품명
+
+    @Column(columnDefinition = "TEXT")
+    private String content;     // 상품설명
+
+    private int price;          // 판매가
+}
