ce33dbc2c3268bbd36416e46c77ed4204b833065
ahah525 <ahah525@naver.com>
2022-10-24 23:34:44 +0900

    #13 - Feat : Spring Security 로 회원 권한 검사, 회원 닉네임이있으면 작가 회원 권한 부여

--- 2Week_Mission/mutbooks/src/main/java/com/example/mutbooks/app/base/security/service/CustomUserDetailsService.java
+++ 2Week_Mission/mutbooks/src/main/java/com/example/mutbooks/app/base/security/service/CustomUserDetailsService.java
@@ -5,13 +5,11 @@ import com.example.mutbooks.app.member.entity.Member;
 import com.example.mutbooks.app.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -23,13 +21,7 @@ public class CustomUserDetailsService implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Member member = memberRepository.findByUsername(username).get();
 
-        List<GrantedAuthority> authorities = new ArrayList<>();
-        authorities.add(new SimpleGrantedAuthority("MEMBER"));
-
-        // 관리자 회원
-        if(member.isAdmin()) {
-            authorities.add(new SimpleGrantedAuthority("ADMIN"));
-        }
+        List<GrantedAuthority> authorities = member.genAuthorities();
 
         return new MemberContext(member, authorities);
     }
