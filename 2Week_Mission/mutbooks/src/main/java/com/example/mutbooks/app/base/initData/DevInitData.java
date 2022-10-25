ffad95b664b93e77d29912e1cc1b2f2b95afd58f
ahah525 <ahah525@naver.com>
2022-10-25 13:16:00 +0900

    #7 - Feat: 1번 회원이 3번 도서 장바구니 추가, 3번 회원이 1, 2 번 도서 장바구니 추가 initDat 추가

--- 2Week_Mission/mutbooks/src/main/java/com/example/mutbooks/app/base/initData/DevInitData.java
+++ 2Week_Mission/mutbooks/src/main/java/com/example/mutbooks/app/base/initData/DevInitData.java
@@ -1,5 +1,6 @@
 package com.example.mutbooks.app.base.initData;
 
+import com.example.mutbooks.app.cart.service.CartService;
 import com.example.mutbooks.app.member.service.MemberService;
 import com.example.mutbooks.app.post.service.PostService;
 import com.example.mutbooks.app.product.service.ProductService;
@@ -12,9 +13,14 @@ import org.springframework.context.annotation.Profile;
 @Profile("dev")
 public class DevInitData implements InitDataBefore {
     @Bean
-    CommandLineRunner initData(MemberService memberService, PostService postService, ProductService productService) {
+    CommandLineRunner initData(
+            MemberService memberService,
+            PostService postService,
+            ProductService productService,
+            CartService cartService
+    ) {
         return args -> {
-            before(memberService, postService, productService);
+            before(memberService, postService, productService, cartService);
         };
     }
 }
--- 2Week_Mission/mutbooks/src/main/java/com/example/mutbooks/app/base/initData/InitDataBefore.java
+++ 2Week_Mission/mutbooks/src/main/java/com/example/mutbooks/app/base/initData/InitDataBefore.java
@@ -1,15 +1,17 @@
 package com.example.mutbooks.app.base.initData;
 
+import com.example.mutbooks.app.cart.service.CartService;
 import com.example.mutbooks.app.member.entity.Member;
 import com.example.mutbooks.app.member.form.JoinForm;
 import com.example.mutbooks.app.member.service.MemberService;
 import com.example.mutbooks.app.post.form.WriteForm;
 import com.example.mutbooks.app.post.service.PostService;
+import com.example.mutbooks.app.product.entity.Product;
 import com.example.mutbooks.app.product.form.ProductForm;
 import com.example.mutbooks.app.product.service.ProductService;
 
 public interface InitDataBefore {
-    default void before(MemberService memberService, PostService postService, ProductService productService) {
+    default void before(MemberService memberService, PostService postService, ProductService productService, CartService cartService) {
         // 1번 회원(작가)
         Member member1 = memberService.join(new JoinForm("user1", "1234", "딸기", "user1@test.com"));
         // 2번 회원(작가)
@@ -25,16 +27,23 @@ public interface InitDataBefore {
         postService.write(member1, new WriteForm("해리포터2-2", "판타지 소설입니다.", "<ul><li><p>판타지 소설입니다.</p></li></ul>", "#해리포터2"));
         postService.write(member1, new WriteForm("해리포터2-3", "판타지 소설입니다.", "<ul><li><p>판타지 소설입니다.</p></li></ul>", "#해리포터2"));
 
-        // 1번 회원이 1번(판타지) 글 키워드 선택 -> 1, 2, 3번 글 도서 등록
-        productService.create(member1, new ProductForm("해리포터1", "판타지 소설입니다.", 18000, 1L, "#판타지 #소설"));
-        // 1번 회원이 1번(판타지) 글 키워드 선택 -> 4, 5, 6번 글 도서 등록
-        productService.create(member1, new ProductForm("해리포터2", "판타지 소설입니다.", 20000, 1L, "#판타지 #소설"));
+        // 1번 회원이 1번(판타지) 글 키워드 선택 -> 1, 2, 3번 글 도서 등록(1번 도서)
+        Product product1 = productService.create(member1, new ProductForm("해리포터1", "판타지 소설입니다.", 18000, 1L, "#판타지 #소설"));
+        // 1번 회원이 1번(판타지) 글 키워드 선택 -> 4, 5, 6번 글 도서 등록(2번 도서)
+        Product product2 = productService.create(member1, new ProductForm("해리포터2", "판타지 소설입니다.", 20000, 1L, "#판타지 #소설"));
 
         // 2번 회원 글 작성
         postService.write(member2, new WriteForm("하트모양 크래커1", "로맨스 소설입니다.", "<ul><li><p>로맨스 소설입니다.</p></li></ul>","#하트모양_크래커"));
         postService.write(member2, new WriteForm("하트모양 크래커2", "로맨스 소설입니다.", "<ul><li><p>로맨스 소설입니다.</p></li></ul>", "#하트모양_크래커"));
 
-        // 2번 회원이 2번(로맨스) 글 키워드 선택 -> 4, 5번 글 도서 등록
-        productService.create(member2, new ProductForm("하트모양 크래커", "로맨스 소설입니다.", 15000, 3L, "#로맨스 #소설"));
+        // 2번 회원이 2번(로맨스) 글 키워드 선택 -> 4, 5번 글 도서 등록(3번 도서)
+        Product product3 = productService.create(member2, new ProductForm("하트모양 크래커", "로맨스 소설입니다.", 15000, 3L, "#로맨스 #소설"));
+
+        // 1번 회원이 3번 도서 장바구니 품목 추가
+        cartService.addCartItem(member1, product3);
+
+        // 3번 회원이 1, 2번 도서 장바구니 품목 추가
+        cartService.addCartItem(member3, product1);
+        cartService.addCartItem(member3, product2);
     }
 }
