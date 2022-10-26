4758951413e3d71d6ac90489c5b1d12207815ec1
ahah525 <ahah525@naver.com>
2022-10-26 15:13:53 +0900

    #18 - Feat: Toss Payments 결제 백엔드 연동

--- 2Week_Mission/mutbooks/src/main/java/com/example/mutbooks/app/order/controller/OrderController.java
+++ 2Week_Mission/mutbooks/src/main/java/com/example/mutbooks/app/order/controller/OrderController.java
@@ -5,16 +5,25 @@ import com.example.mutbooks.app.member.entity.Member;
 import com.example.mutbooks.app.member.service.MemberService;
 import com.example.mutbooks.app.order.entity.Order;
 import com.example.mutbooks.app.order.service.OrderService;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.*;
+import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.ResponseErrorHandler;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.server.ResponseStatusException;
 
+import javax.annotation.PostConstruct;
+import java.util.Base64;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
@@ -22,6 +31,8 @@ import java.util.List;
 public class OrderController {
     private final OrderService orderService;
     private final MemberService memberService;
+    private final RestTemplate restTemplate = new RestTemplate();
+    private final ObjectMapper objectMapper;    // Ut
 
     // 주문 생성
     @PreAuthorize("isAuthenticated()")
@@ -82,4 +93,74 @@ public class OrderController {
         // 주문 내역 페이지로 리다이렉트
         return "redirect:/order/list";
     }
+
+
+    // Toss Payments 시작
+    @PostConstruct
+    private void init() {
+        restTemplate.setErrorHandler(new ResponseErrorHandler() {
+            @Override
+            public boolean hasError(ClientHttpResponse response) {
+                return false;
+            }
+
+            @Override
+            public void handleError(ClientHttpResponse response) {
+            }
+        });
+    }
+
+    private final String SECRET_KEY = "test_sk_jkYG57Eba3GlOkbXE5lVpWDOxmA1";
+
+    // 결제 성공 리다이렉트 URL
+    @RequestMapping("/{id}/success")
+    public String confirmPayment(
+            @RequestParam String paymentKey,    // 결제 건에 대한 고유한 키 값
+            @RequestParam String orderId,       // 상점에서 주문 건 구분을 위해 발급한 고유ID
+            @RequestParam Long amount,          // 실 결제 금액
+            Model model) throws Exception {
+
+        HttpHeaders headers = new HttpHeaders();
+        // headers.setBasicAuth(SECRET_KEY, ""); // spring framework 5.2 이상 버전에서 지원
+        headers.set("Authorization", "Basic " + Base64.getEncoder().encodeToString((SECRET_KEY + ":").getBytes()));
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        Map<String, String> payloadMap = new HashMap<>();
+        payloadMap.put("orderId", orderId);
+        payloadMap.put("amount", String.valueOf(amount));
+
+        // TODO : 주문 금액 검증 로직
+
+        HttpEntity<String> request = new HttpEntity<>(objectMapper.writeValueAsString(payloadMap), headers);
+
+        ResponseEntity<JsonNode> responseEntity = restTemplate.postForEntity(
+                "https://api.tosspayments.com/v1/payments/" + paymentKey, request, JsonNode.class);
+
+        if (responseEntity.getStatusCode() == HttpStatus.OK) {
+            JsonNode successNode = responseEntity.getBody();
+            model.addAttribute("orderId", successNode.get("orderId").asText());
+            String secret = successNode.get("secret").asText(); // 가상계좌의 경우 입금 callback 검증을 위해서 secret을 저장하기를 권장함
+            return "order/success";
+        } else {
+            JsonNode failNode = responseEntity.getBody();
+            model.addAttribute("message", failNode.get("message").asText());
+            model.addAttribute("code", failNode.get("code").asText());
+            return "order/fail";
+        }
+    }
+
+    // 결제 실패 리다패렉트 URL
+    @RequestMapping("/{id}/fail")
+    public String failPayment(
+            @RequestParam String message,   // 에러 메시지
+            @RequestParam String code,      // 에러 코드
+            @RequestParam String orderId,   // 상점에서 주문 건 구분을 위해 발급한 고유ID
+            Model model) {
+        model.addAttribute("message", message);
+        model.addAttribute("code", code);
+        model.addAttribute("orderId", orderId);
+
+        return "order/fail";
+    }
+    // Toss Payments 끝
 }
