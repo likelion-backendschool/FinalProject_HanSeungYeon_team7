8336b7f91d4b14bb8827a43cf367552e9e4af5f1
ahah525 <ahah525@naver.com>
2022-11-01 17:47:22 +0900

    #23 - Feat: 출금 신청폼 페이지에 출금 계좌 정보 표시

--- 3Week_Mission/mutbooks/src/main/java/com/example/mutbooks/app/withdraw/controller/WithdrawController.java
+++ 3Week_Mission/mutbooks/src/main/java/com/example/mutbooks/app/withdraw/controller/WithdrawController.java
@@ -1,11 +1,13 @@
 package com.example.mutbooks.app.withdraw.controller;
 
 import com.example.mutbooks.app.base.security.dto.MemberContext;
+import com.example.mutbooks.app.member.entity.Member;
 import com.example.mutbooks.app.withdraw.form.WithdrawApplyForm;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -16,7 +18,9 @@ public class WithdrawController {
     // 출금 신청 폼
     @PreAuthorize("isAuthenticated()")
     @GetMapping("/apply")
-    public String showApply(@AuthenticationPrincipal MemberContext memberContext, WithdrawApplyForm withdrawApplyForm) {
+    public String showApply(@AuthenticationPrincipal MemberContext memberContext, WithdrawApplyForm withdrawApplyForm, Model model) {
+        Member member = memberContext.getMember();
+        model.addAttribute("member", member);
 
         return "withdraw/apply";
     }
