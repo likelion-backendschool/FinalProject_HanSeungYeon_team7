0dd7bab4874b527275d23684dd1f8b738cfd5575
ahah525 <ahah525@naver.com>
2022-11-08 10:56:55 +0900

    #2 - Feat: RsData(REST API 용 ResponseEntity Body DTO) 도입

---
+++ 4Week_Mission/mutbooks/src/main/java/com/example/mutbooks/app/base/dto/RsData.java
@@ -0,0 +1,26 @@
+package com.example.mutbooks.app.base.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class RsData<T> {
+    private String resultCode;
+    private String msg;
+    private T data;
+
+    public static <T> RsData<T> of(String resultCode, String msg) {
+        return new RsData<>(resultCode, msg, null);
+    }
+
+    public boolean isSuccess() {
+        return resultCode.startsWith("S-1");
+    }
+
+    public boolean isFail() {
+        return isSuccess() == false;
+    }
+}
