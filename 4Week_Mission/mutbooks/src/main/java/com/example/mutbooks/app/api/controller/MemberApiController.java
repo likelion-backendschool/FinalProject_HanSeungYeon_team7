96bf825b4b808a07b45d014bb3e152f2612667dd
ahah525 <ahah525@naver.com>
2022-11-08 10:03:51 +0900

    #33 - Feat: HttpServletResponse 헤더에 임시 Authentication 값을 담아 응답 보내기

--- 4Week_Mission/mutbooks/src/main/java/com/example/mutbooks/app/api/controller/MemberApiController.java
+++ 4Week_Mission/mutbooks/src/main/java/com/example/mutbooks/app/api/controller/MemberApiController.java
@@ -6,12 +6,16 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
+
 @RestController
 @RequestMapping("/api/v1/member")
 public class MemberApiController {
 
     @PostMapping("/login")
-    public String login(@RequestBody LoginDto loginDto) {
+    public String login(@RequestBody LoginDto loginDto, HttpServletResponse response) {
+        response.addHeader("Authentication", "JWT Token");
+
         return "username : %s, password : %s".formatted(loginDto.getUsername(), loginDto.getPassword());
     }
 }
