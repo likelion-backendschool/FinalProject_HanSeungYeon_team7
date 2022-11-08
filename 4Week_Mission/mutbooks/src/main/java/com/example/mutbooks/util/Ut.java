886bdf280f09f2edd83461c9d9138ff366530792
ahah525 <ahah525@naver.com>
2022-11-08 11:11:25 +0900

    #2 - Feat: Ut.mapOf() 도입

--- 4Week_Mission/mutbooks/src/main/java/com/example/mutbooks/util/Ut.java
+++ 4Week_Mission/mutbooks/src/main/java/com/example/mutbooks/util/Ut.java
@@ -10,8 +10,28 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class Ut {
+    // 인자 값들을 map 형태로 반환
+    public static<K, V> Map<K, V> mapOf(Object... args) {
+        Map<K, V> map = new LinkedHashMap<>();
+
+        int size = args.length / 2;
+
+        for (int i = 0; i < size; i++) {
+            int keyIndex = i * 2;
+            int valueIndex = keyIndex + 1;
+
+            K key = (K) args[keyIndex];
+            V value = (V) args[valueIndex];
+
+            map.put(key, value);
+        }
+        return map;
+    }
+
     public static class spring {
         public static <T> ResponseEntity<RsData> responseEntityOf(RsData<T> rsData) {
             return responseEntityOf(rsData, null);
