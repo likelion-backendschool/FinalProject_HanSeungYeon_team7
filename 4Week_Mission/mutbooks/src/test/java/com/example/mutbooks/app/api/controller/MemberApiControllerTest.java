60aa1cef9f150fed1e972cf6449e0346f1f129a5
ahah525 <ahah525@naver.com>
2022-11-08 10:00:04 +0900

    #34 - Test: 올바른 POST /api/v1/member/login 요청의 응답 헤더에 Authentication 값(JWT 토큰)이 있는지 테스트

--- 4Week_Mission/mutbooks/src/test/java/com/example/mutbooks/app/api/controller/MemberApiControllerTest.java
+++ 4Week_Mission/mutbooks/src/test/java/com/example/mutbooks/app/api/controller/MemberApiControllerTest.java
@@ -6,13 +6,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.nio.charset.StandardCharsets;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -46,5 +49,36 @@ class MemberApiControllerTest {
         // Then
         resultActions
                 .andExpect(status().is2xxSuccessful());
+    }
+
+    @Test
+    @DisplayName("POST /api/v1/member/login 으로 올바른 username과 password 데이터를 넘기면 JWT키를 발급해 응답 헤더에 담아준다.")
+    void t2() throws Exception {
+        // When
+        ResultActions resultActions = mvc
+                .perform(
+                        post("/api/v1/member/login")
+                                .content("""
+                                        {
+                                            "username": "user1",
+                                            "password": "1234"
+                                        }
+                                        """.stripIndent())
+                                .contentType(new MediaType(MediaType.APPLICATION_JSON, StandardCharsets.UTF_8))
+                )
+                .andDo(print());
+
+        // Then
+        resultActions
+                .andExpect(status().is2xxSuccessful());
+
+        // 응답 결과
+        MvcResult mvcResult = resultActions.andReturn();
+
+        MockHttpServletResponse response = mvcResult.getResponse();
+        // 응답 헤더 Authentication(key) : JWT 키(value)
+        String authentication = response.getHeader("Authentication");
+
+        assertThat(authentication).isNotEmpty();
     }
 }
