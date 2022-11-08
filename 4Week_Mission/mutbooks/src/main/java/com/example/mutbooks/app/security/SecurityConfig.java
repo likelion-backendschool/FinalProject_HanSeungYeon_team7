50fe75fd31276b59527508f4494b4ccdfa72e16f
ahah525 <ahah525@naver.com>
2022-11-08 23:04:16 +0900

    #33 - spring doc 의존성 추가, SpringDocConfig 추가, spring doc 관리자 회원만 접근가능하도록 security 설정

--- 4Week_Mission/mutbooks/src/main/java/com/example/mutbooks/app/security/SecurityConfig.java
+++ 4Week_Mission/mutbooks/src/main/java/com/example/mutbooks/app/security/SecurityConfig.java
@@ -19,12 +19,21 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .httpBasic().disable()
                 .formLogin(
                         formLogin -> formLogin
                                 .loginPage("/member/login") // GET
                                 .loginProcessingUrl("/member/login") // POST
 //                                .successHandler(authenticationSuccessHandler)
 //                                .failureHandler(authenticationFailureHandler)
+                )
+                .authorizeRequests(
+                        authorizeRequests -> authorizeRequests
+                                // spring doc 관리자 회원만 허용
+                                .antMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html")
+                                .hasAuthority("ADMIN")
+                                .anyRequest()
+                                .permitAll()
                 )
                 .logout(
                         logout -> logout.logoutUrl("/member/logout")
