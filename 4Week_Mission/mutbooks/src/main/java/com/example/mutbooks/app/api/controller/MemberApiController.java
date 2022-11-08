058a9354da5928ec7d41aee88f9d161c1d9ab610
ahah525 <ahah525@naver.com>
2022-11-08 10:08:11 +0900

    #33 - Feat: ResponseEntity 로 응답 헤더, 바디, 상태코드 설정하여 응답 보내는 것으로 수정

--- 4Week_Mission/mutbooks/src/main/java/com/example/mutbooks/app/api/controller/MemberApiController.java
+++ 4Week_Mission/mutbooks/src/main/java/com/example/mutbooks/app/api/controller/MemberApiController.java
@@ -1,21 +1,26 @@
 package com.example.mutbooks.app.api.controller;
 
 import com.example.mutbooks.app.member.dto.LoginDto;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpServletResponse;
 
 @RestController
 @RequestMapping("/api/v1/member")
 public class MemberApiController {
 
     @PostMapping("/login")
-    public String login(@RequestBody LoginDto loginDto, HttpServletResponse response) {
-        response.addHeader("Authentication", "JWT Token");
+    public ResponseEntity<String> login(@RequestBody LoginDto loginDto) {
+        // 헤더(Authentication) 에 JWT 토큰 & 바디에 username, password
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authentication", "JWT Token");
 
-        return "username : %s, password : %s".formatted(loginDto.getUsername(), loginDto.getPassword());
+        String body = "username : %s, password : %s".formatted(loginDto.getUsername(), loginDto.getPassword());
+
+        return new ResponseEntity<>(body, headers, HttpStatus.OK);
     }
 }
